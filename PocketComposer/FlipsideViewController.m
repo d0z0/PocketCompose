1290a8c320d96cdbbd2e8671a068d9724ec305fe
Schubert Cardozo <schubertcardozo@Schuberts-MacBook-Pro.local>
2013-11-01 21:17:02 +0530

    Initial Commit

---
+++ PocketComposer/FlipsideViewController.m
@@ -0,0 +1,36 @@
+//
+//  FlipsideViewController.m
+//  PocketComposer
+//
+//  Created by Schubert Cardozo on 01/11/13.
+//  Copyright (c) 2013 Schubert Cardozo. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface FlipsideViewController ()
+
+@end
+
+@implementation FlipsideViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (IBAction)done:(id)sender
+{
+    [self.delegate flipsideViewControllerDidFinish:self];
+}
+
+@end
---
+++ PocketComposer/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  PocketComposer
+//
+//  Created by Schubert Cardozo on 01/11/13.
+//  Copyright (c) 2013 Schubert Cardozo. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@end
